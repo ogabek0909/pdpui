fb405a9a98c2860385d7b83f3f935330e99c1090
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-12-25 15:30:32 +0500

    ready application

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'USER',
+              style: TextStyle(
+                fontSize: 27,
+                color: Colors.red,
+              ),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Text(
+              'INTERFACE',
+              style: TextStyle(
+                fontSize: 27,
+                color: Colors.green,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
